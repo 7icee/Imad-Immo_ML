2cf56a24b7b6b8fb749dda8c34a159e6b9de39e9
Imad <f.e.imad@hotmail.com>
2024-12-18 14:36:29 +0100

    Update Step_4_App.py

--- Data_ml/Step_4_App.py
+++ Data_ml/Step_4_App.py
@@ -4,8 +4,8 @@ import joblib
 import numpy as np
 
 # Load the saved model, scaler, and additional data
-model = joblib.load(r'Data_ml/Step_4_Linear_Regression_Model.pkl')
-scaler = joblib.load('Data_ml\Step_4_Scaler.pkl')
+model = joblib.load('Step_4_Linear_Regression_Model.pkl')
+scaler = joblib.load('Step_4_Scaler.pkl')
 wealth_and_density_data = pd.read_excel('Step_4_Postal_Codes_Data.xlsx')
 
 # Function to retrieve wealth index and density based on postal code
